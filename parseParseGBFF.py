a60324c6d9beac3d37f59dc092a4e990067dbafc
dmkuzsib <Dmitry.Kuznetsov@sib.swiss>
2024-06-20 17:42:04 +0200

    Create parseParseGBFF.py
    additional parsing of file id.org.tab creted by parseGBFF.py

---
+++ parseParseGBFF.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3.6
+
+import sys
+from optparse import OptionParser
+
+parser = OptionParser(usage='Usage: '+sys.argv[0]+'/path/to/id.org.tab, the file created by parseGBFF.py', description='Parses output of parseGBFF.py')
+parser.add_option("-d", "--debug", dest="doDebug", action='store_true', default=False, help='debug')
+
+(options, args) = parser.parse_args(args=None, values=None)
+if options.doDebug: print ('-----Options------'); print(args); print(options)
+
+#print(options)
+#print(args)
+
+if len(args) != 1:
+	parser.print_help()
+	sys.exit()
+
+import pandas
+import datetime
+import json
+from dateutil.parser import parse
+#from zipfile import ZipFile
+
+
+def CustomParser(data):
+	j1 = json.loads(data)
+	return j1
+
+df = pandas.read_csv(args[0],sep='\t',converters={4:CustomParser},header=None)
+epoch = parse('1000-01-01')
+for i,row in df.iterrows():
+	if options.doDebug: print (i,row)
+	df.loc[i,'pathogen_tax_id']=row[1]
+	df.loc[i,'collection_date']=parse(row[4]['collection_date'],fuzzy=True, default=epoch) if 'collection_date' in row[4] else None
+	df.loc[i,'country']=row[4]['country'] if 'country' in row[4] else None
+	df.loc[i,'host']=row[4]['host'] if 'host' in row[4] else None
+	if 'strain' in row[4]:
+		df.loc[i,'isolate']=row[4]['strain']
+	elif 'isolate' in row[4]:
+		df.loc[i,'isolate']=row[4]['isolate']
+
+#	df.loc[i,'strain']=row[4]['strain'] if 'strain' in row[4] else None
+df_ = df[~df['collection_date'].isnull() & ~df['country'].isnull() & ~df['host'].isnull()]
+#print(df)
+#print(df_)
+df_.to_csv(sys.stdout, columns=['pathogen_tax_id','collection_date','country','host','isolate'], index=False)
+
+# FF=open('filtered.fasta','w')
+# for filename in ('fasta/DNA/'+df_[0]).tolist():
+# 	f=open(filename,'r')
+# 	FF.write(f.read())
+# 	f.close()
+# FF.close()
+#	f=open('fasta/DNA/'+row[0])
