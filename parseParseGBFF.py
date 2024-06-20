2b1abe46ea33d520a1a1b33e44c4d2619f09a117
dmkuzsib <Dmitry.Kuznetsov@sib.swiss>
2024-06-20 19:22:06 +0200

    Update parseParseGBFF.py

--- parseParseGBFF.py
+++ parseParseGBFF.py
@@ -5,6 +5,7 @@ from optparse import OptionParser
 
 parser = OptionParser(usage='Usage: '+sys.argv[0]+'/path/to/id.org.tab, the file created by parseGBFF.py', description='Parses output of parseGBFF.py')
 parser.add_option("-d", "--debug", dest="doDebug", action='store_true', default=False, help='debug')
+parser.add_option("-1", "--header", dest="outHeader", action='store_true', default=False, help='Add header 1st row')
 
 (options, args) = parser.parse_args(args=None, values=None)
 if options.doDebug: print ('-----Options------'); print(args); print(options)
@@ -27,14 +28,16 @@ def CustomParser(data):
 	j1 = json.loads(data)
 	return j1
 
-df = pandas.read_csv(args[0],sep='\t',converters={4:CustomParser},header=None)
+df = pandas.read_csv(args[0],sep='\t',dtype={1:str},converters={4:CustomParser},header=None)
 epoch = parse('1000-01-01')
 for i,row in df.iterrows():
 	if options.doDebug: print (i,row)
 	df.loc[i,'pathogen_tax_id']=row[1]
+	df.loc[i,'pathogen']=row[4]['organism'] if 'organism' in row[4] else None
 	df.loc[i,'collection_date']=parse(row[4]['collection_date'],fuzzy=True, default=epoch) if 'collection_date' in row[4] else None
 	df.loc[i,'country']=row[4]['country'] if 'country' in row[4] else None
 	df.loc[i,'host']=row[4]['host'] if 'host' in row[4] else None
+	df.loc[i,'serotype']=row[4]['serotype'] if 'serotype' in row[4] else None
 	if 'strain' in row[4]:
 		df.loc[i,'isolate']=row[4]['strain']
 	elif 'isolate' in row[4]:
@@ -44,7 +47,7 @@ for i,row in df.iterrows():
 df_ = df[~df['collection_date'].isnull() & ~df['country'].isnull() & ~df['host'].isnull()]
 #print(df)
 #print(df_)
-df_.to_csv(sys.stdout, columns=['pathogen_tax_id','collection_date','country','host','isolate'], index=False)
+df_.to_csv(sys.stdout, columns=['pathogen_tax_id','pathogen','collection_date','country','host','serotype','isolate'], index=False, header=options.outHeader)
 
 # FF=open('filtered.fasta','w')
 # for filename in ('fasta/DNA/'+df_[0]).tolist():
